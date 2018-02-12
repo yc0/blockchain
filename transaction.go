a53712db7cdb730accf02a9bb0e8e77e297bdb72
yc0 <nelson.lin@pushme.com.tw>
2018-02-12 11:37:49 +0800

    add NewUTXOTransaction method to do transaction recorded from , to and amount

--- transaction.go
+++ transaction.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"encoding/gob"
+	"encoding/hex"
 	"log"
 )
 
@@ -66,4 +67,41 @@ func (in *TXInput) CanUnlockOutputWith(unlockingData string) bool {
 
 func (out *TXOutput) CanBeUnlockedWith(unlockingData string) bool {
 	return out.ScriptPubKey == unlockingData
+}
+
+func NewUTXOTransaction(from, to string, amount int, bc *BlockChain) *Transaction {
+	var inputs []TXInput
+	var outputs []TXOutput
+	// Before creating new outputs, we first have to find all unspent outputs
+	// and ensure that they store enough value.
+	// This is what FindSpendableOutputs
+	acc, validOutputs := bc.FindSpendableOutputs(from, amount)
+	if acc < amount {
+		log.Panic("Error: Not enough funds")
+	}
+
+	// Build a list of inputs
+	for txid, outs := range validOutputs {
+		_txid, _ := hex.DecodeString(txid)
+		for _, out := range outs {
+			input := TXInput{_txid, out, from}
+			inputs = append(inputs, input)
+		}
+	}
+
+	// Build a list of outputs
+	// One that’s locked with the receiver address.
+	// This is the actual transferring of coins to other address
+	outputs = append(outputs, TXOutput{amount, to})
+	if acc > amount {
+		// One that’s locked with the sender address.
+		// This is a change.
+		// It’s only created when unspent outputs hold more value than required
+		// for the new transaction.
+		// Remember: outputs are indivisible.
+		outputs = append(outputs, TXOutput{amount - acc, from}) // a change
+	}
+	tx := &Transaction{nil, inputs, outputs}
+	tx.SetID()
+	return tx
 }
